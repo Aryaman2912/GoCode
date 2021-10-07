b0fb77216295a6104ce6d90a5f5b69c0e3cb47e4
Aryaman2912 <aryavnps@gmail.com>
2021-10-07 09:57:46 +0530

    signup works

---
+++ backend/middleware/auth.js
@@ -0,0 +1,33 @@
+import jwt from 'jsonwebtoken';
+// Middleware to ensure that when a POST request is made, the user making
+// The POST/PUT request is authenticated
+//
+// Flow is as follows:
+// User makes a POST/PUT/DELETE request => Middleware is invoked to authenticate user => next() is invoked to continue the request.
+const auth = async (req, res, next) => {
+    try {
+        const token = req.headers.authorization.split(" ")[1];
+        const isCustomAuth = token.length < 500;
+
+        let decodedData;
+
+        if(isCustomAuth && token) {
+            // For custom authentication tokens. 
+            decodedData = jwt.verify(token, 'randomaccesstoken');
+
+            req.userId = decodedData?.id;
+        } else {
+            // For tokens obtained using oauth. 
+            decodedData = jwt.decode(token);
+
+            req.userId = decodedData?.sub;
+        }
+
+        next();
+    } catch(err) {
+        // console.log('F');
+        console.log(err);
+    }
+}
+
+export default auth;
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,16 +2,23 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+// import reportWebVitals from './reportWebVitals';
+import {createStore, applyMiddleware, compose } from 'redux';
+import {reducers } from './reducers'
+import thunk  from 'redux-thunk';
+import { Provider } from 'react-redux';
+const store = createStore(reducers,compose(applyMiddleware(thunk)));
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
+  </Provider>,
   document.getElementById('root')
 );
+
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// reportWebVitals();
