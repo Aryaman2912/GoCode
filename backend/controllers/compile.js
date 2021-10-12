dac78ac60c70d02f0d75ceb326df710ee6ee4abb
srujangit123 <srujantmk@gmail.com>
2021-10-12 11:01:54 +0530

    Add basic compiler code

--- backend/controllers/compile.js
+++ backend/controllers/compile.js
@@ -1,6 +1,14 @@
-export const problemCompilation = (req, res) => {
+import axios from 'axios';
+
+
+export const problemCompilation = async(req, res) => {
     const { code, language } = req.body;
-    console.log(code)
-    // TODO -> Code compiliation
-    res.send("Problem received")
+    const input = {
+        "script": code,
+        "language": "cpp",
+        "clientId": "e67139a8317554984daafaa1fce69d93",
+        "clientSecret": "4a231fff8bf76c4f40e427ca7551cf5c96d67ab61d835307f478c78b7ff70ec4",
+    }
+    const resp = await axios.post('https://api.jdoodle.com/v1/execute', input)
+    res.send(resp.data);
 }
