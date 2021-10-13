325ddadf8ed237ccff77c9bcf442a95198e8ade4
srujangit123 <srujantmk@gmail.com>
2021-10-13 19:26:29 +0530

    Add user input output feature

--- backend/controllers/compile.js
+++ backend/controllers/compile.js
@@ -2,12 +2,14 @@ import axios from 'axios';
 
 
 export const problemCompilation = async(req, res) => {
-    const { code, language } = req.body;
+    const { code, language, userInput } = req.body;
+    console.log(userInput)
     const input = {
         "script": code,
         "language": "cpp",
         "clientId": "e67139a8317554984daafaa1fce69d93",
         "clientSecret": "4a231fff8bf76c4f40e427ca7551cf5c96d67ab61d835307f478c78b7ff70ec4",
+        "stdin": userInput
     }
     const resp = await axios.post('https://api.jdoodle.com/v1/execute', input)
     res.send(resp.data);
