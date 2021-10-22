2ef480f16273898df281a00b071e778e7caa7cd7
Rashmith <rashmiths.191cs242@nitk.edu.in>
2021-10-22 22:23:12 +0530

    add contest page

--- frontend/src/components/ContestSpace/AddProblem.jsx
+++ frontend/src/components/ContestSpace/AddProblem.jsx
@@ -1,42 +1,116 @@
 import React from "react";
 import { useForm } from "react-hook-form";
-import {Container} from '@material-ui/core';
+import { Container } from "@material-ui/core";
 import useStyles from "./styles";
-import Select from 'react-select';
+import Select from "react-select";
+
+class Challenge {
+  constructor(
+    name,
+    description,
+    statement,
+    tags,
+    input,
+    output,
+    testinput,
+    testoutput
+  ) {
+    this.name = name;
+    this.description = description;
+    this.statement = statement;
+    this.tags = tags;
+    this.input = input;
+    this.output = output;
+    this.testinput = testinput;
+    this.testoutput = testoutput;
+  }
+}
 const AddContest = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+  const onSubmit = (data) => {
+    console.log(data);
+  };
 
-    const {
-        register,
-        handleSubmit,
-        formState: { errors }
-    } = useForm();
-    const onSubmit = (data) => {
-        console.log(data);
-    };
+  const classes = useStyles();
 
-    const classes = useStyles();
-
-    const tags = [
-        { value: 'chocolate', label: 'Chocolate' },
-        { value: 'strawberry', label: 'Strawberry' },
-        { value: 'vanilla', label: 'Vanilla' }
-      ]
+  const tags = [
+    { value: "flows", label: "flows" },
+    { value: "graph matchings", label: "graph matchings" },
+    { value: "graphs", label: "graphs" },
+    { value: "greedy", label: "greedy" },
+    { value: "binary search", label: "binary search" },
+    { value: "dp", label: "dp" },
+    { value: "geometry", label: "geometry" },
+    { value: "brute force", label: "brute force" },
+    { value: "data structures", label: "data structures" },
+    { value: "implementation", label: "implementation" },
+    { value: "sortings", label: "sortings" },
+    { value: "constructive algorithms", label: "constructive algorithms" },
+    { value: "two pointers", label: "two pointers" },
+    { value: "dfs and similar", label: "dfs and similar" },
+    { value: "hashing", label: "hashing" },
+    { value: "bitmasks", label: "bitmasks" },
+    { value: "meet-in-the-middle", label: "meet-in-the-middle" },
+    { value: "combinatorics", label: "combinatorics" },
+    { value: "math", label: "math" },
+    { value: "dsu", label: "dsu" },
+    { value: "divide and conquer", label: "divide and conquer" },
+    { value: "strings", label: "strings" },
+    { value: "interactive", label: "interactive" },
+    { value: "number theory", label: "number theory" },
+    { value: "shortest paths", label: "shortest paths" },
+    { value: "trees", label: "trees" },
+    { value: "probabilities", label: "probabilities" },
+    { value: "string suffix structures", label: "string suffix structures" },
+    { value: "fft", label: "fft" },
+    { value: "matrices", label: "matrices" },
+    { value: "2-sat", label: "2-sat" },
+    { value: "ternary search", label: "ternary search" },
+    { value: "games", label: "games" },
+    { value: "chinese remainder theorem", label: "chinese remainder theorem" },
+    { value: "dfs", label: "dfs" },
+    { value: "Array", label: "Array" },
+    { value: "trie", label: "trie" },
+    { value: "linked list", label: "linked list" },
+    { value: "sliding window", label: "sliding window" },
+    { value: "stack", label: "stack" },
+    { value: "queue", label: "queue" },
+  ];
+  let ch = new Challenge();
   return (
     <Container className={classes.container} maxWidth="sm">
-    <h1 className={classes.h1}>Create Problem</h1>
-    <form className = {classes.form}onSubmit={handleSubmit(onSubmit)}>
-          <label className= {classes.label} htmlFor="contestName">Problem Name:</label>
-          <input className= {classes.input}
-              {...register("problemName", { required: "This is required." })}
-              id="problemName" />
-          {errors.firstName && <p className={classes.p}>{errors.firstName.message}</p>}
-          <label className={classes.label} htmlFor="problemStatement">Problem Statement: </label>
-          <textarea name="problemStatement" placeholder="Enter the problem statement" className={classes.input}></textarea>
-          <label className={classes.label} htmlFor="tags">Tags: </label>
-          <Select className = {classes.dropdown} isMulti options={tags}/>
-          <input className= {classes.submitButton} value="Next" type="submit" />
+      <h1 className={classes.h1}>Create Problem</h1>
+      <form className={classes.form} onSubmit={handleSubmit(onSubmit)}>
+        <label className={classes.label} htmlFor="contestName">
+          Problem Name:
+        </label>
+        <input
+          className={classes.input}
+          {...register("problemName", { required: "This is required." })}
+          id="problemName"
+        />
+        {errors.firstName && (
+          <p className={classes.p}>{errors.firstName.message}</p>
+        )}
+        <label className={classes.label} htmlFor="problemStatement">
+          Problem Statement:{" "}
+        </label>
+        <textarea
+          name="problemStatement"
+          placeholder="Enter the problem statement"
+          className={classes.input}
+        ></textarea>
+        <label className={classes.label} htmlFor="tags">
+          Tags:{" "}
+        </label>
+        <Select className={classes.dropdown} isMulti options={tags} />
+        <input className={classes.submitButton} value="Next" type="submit" />
       </form>
-      </Container>
+    </Container>
   );
 };
 
--- frontend/src/components/ContestSpace/ContestSpace.jsx
+++ frontend/src/components/ContestSpace/ContestSpace.jsx
@@ -1,68 +1,225 @@
-import {useEffect, useState} from 'react'
-import { Typography } from '@material-ui/core';
-import ReactLoading from 'react-loading';
-import Contest from './Contest';
+import { useEffect, useState, React } from "react";
+import { useHistory } from "react-router-dom";
+import { Typography } from "@material-ui/core";
+import ReactLoading from "react-loading";
+import Contest from "./Contest";
+import Button from "@material-ui/core/Button";
+import AddIcon from "@material-ui/icons/Add";
+import { Row } from "react-grid-system";
+import TextField from "@material-ui/core/TextField";
+import Dialog from "@material-ui/core/Dialog";
+import DialogActions from "@material-ui/core/DialogActions";
+import DialogContent from "@material-ui/core/DialogContent";
+import DialogContentText from "@material-ui/core/DialogContentText";
+import DialogTitle from "@material-ui/core/DialogTitle";
+import useStyles from "./styles";
+import { useForm } from "react-hook-form";
 
 const ContestSpace = () => {
-
-    const [contests, setContests] = useState([])
-    const [loading, setLoading] = useState(true)
-    useEffect(() => {
-        fetch('http://localhost:5000/api/contests')
-        .then((data) => data.json())
-        .then(data => {
-            let tempcontests = [];
-            data.forEach(contest => {
-                if (contest.isPublic) {
-                    tempcontests.push(contest);
-                }
-            });
-          setContests(tempcontests);
-            setLoading(false);
+  const [contests, setContests] = useState([]);
+  const [loading, setLoading] = useState(true);
+  useEffect(() => {
+    fetch("http://localhost:5000/api/contests")
+      .then((data) => data.json())
+      .then((data) => {
+        let tempcontests = [];
+        data.forEach((contest) => {
+          if (contest.isPublic) {
+            tempcontests.push(contest);
+          }
         });
-        
-    }, [])
+        setContests(tempcontests);
+        setLoading(false);
+      });
+  }, []);
 
-    console.log(contests);
+  console.log(contests);
 
-    const loadingOptions = {
-      type: "spin",
-      color: "#347deb",
-  }
-    return (
-      <>
-      {loading ? <div style={{
-          display: 'flex',
-          justifyContent: 'center',
-          alignItems: 'center',
-          height: '90vh'
+  const [open, setOpen] = useState(false);
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
 
-      }}><ReactLoading type={loadingOptions.type} color={loadingOptions.color} height={100} width={100} /></div> :
-          <>
-     
-        <div>
+  const handleClose = () => {
+    setOpen(false);
+  };
+  const history = useHistory();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+  const onSubmit = (data) => {
+    history.push("/addcontest");
+    console.log(data);
+  };
+
+  const classes = useStyles();
+  const loadingOptions = {
+    type: "spin",
+    color: "#347deb",
+  };
+
+  return (
+    <>
+      {loading ? (
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            height: "90vh",
+          }}
+        >
+          <ReactLoading
+            type={loadingOptions.type}
+            color={loadingOptions.color}
+            height={100}
+            width={100}
+          />
+        </div>
+      ) : (
+        <>
+          <div>
+            <div style={{ display: "flex", justifyContent: "flex-end" }}>
+              <Button
+                style={{
+                  color: "white",
+                  padding: "1rem 2rem ",
+                  borderColor: "white",
+                  marginLeft: "auto",
+                  background: "#006633",
+                }}
+                variant="contained"
+                onClick={handleClickOpen}
+              >
+                <Row>
+                  <AddIcon />
+                  <Typography
+                    style={{
+                      marginLeft: "10px",
+                    }}
+                  >
+                    Create Contest
+                  </Typography>
+                </Row>
+              </Button>
+              <Dialog
+                open={open}
+                onClose={handleClose}
+                aria-labelledby="form-dialog-title"
+              >
+                <DialogTitle id="form-dialog-title">Create Contest</DialogTitle>
+                <DialogContent
+                  style={{
+                    width: "35rem",
+                  }}
+                >
+                  <DialogContentText></DialogContentText>
+                  <form
+                    className={classes.form}
+                    onSubmit={handleSubmit(onSubmit)}
+                  >
+                    <label className={classes.label} htmlFor="contestName">
+                      Contest Name:
+                    </label>
+
+                    <input
+                      className={classes.input}
+                      {...register("contestName", {
+                        required: "Contest name cannot be empty.",
+                      })}
+                      id="contestName"
+                    />
+                    {errors.contestName && (
+                      <span className={classes.p}>
+                        {errors.contestName.message}
+                      </span>
+                    )}
+
+                    <label className={classes.label} htmlFor="date">
+                      Date:
+                    </label>
+                    <input
+                      className={classes.input}
+                      type="date"
+                      {...register("date", {
+                        required: "Contest Date cannot be empty.",
+                        valueAsDate: true,
+                      })}
+                    />
+                    {errors.date && (
+                      <span className={classes.p}>{errors.date.message}</span>
+                    )}
+                    <label className={classes.label} htmlFor="time">
+                      Start Time:
+                    </label>
+                    <input
+                      className={classes.input}
+                      type="time"
+                      {...register("time", {
+                        required: true,
+                        valueAsTime: true,
+                      })}
+                    />
+                    <label className={classes.label} htmlFor="duration">
+                      Duration(hrs)
+                    </label>
+                    <input
+                      className={classes.input}
+                      type="number"
+                      step="0.5"
+                      {...register("duration", {
+                        required: "Contest Duration cannot be empty.",
+                        valueAsNumber: true,
+                      })}
+                      id="duration"
+                    />
+                    {errors.duration && (
+                      <span className={classes.p}>
+                        {errors.duration.message}
+                      </span>
+                    )}
+                    <input
+                      className={classes.submitButton}
+                      value="Next"
+                      type="submit"
+                    />
+                  </form>
+                </DialogContent>
+                <DialogActions>
+                  <Button onClick={handleClose} color="primary">
+                    Cancel
+                  </Button>
+                  {/* <Button onClick={handleClose} color="primary">
+                    Subscribe
+                  </Button> */}
+                </DialogActions>
+              </Dialog>
+            </div>
             <Typography
-           variant='h4' style={{
-           
-            color: 'white'}}
+              variant="h4"
+              style={{
+                color: "white",
+              }}
             >
               Public Contests
             </Typography>
-            <Contest  data={contests}/>
+
+            <Contest data={contests} />
             <Typography
-           variant='h4' style={{
-           
-            color: 'white'}}
+              variant="h4"
+              style={{
+                color: "white",
+              }}
             >
               Invited Contests
             </Typography>
-        </div>
+          </div>
         </>
-         }
-         </>
-
-    );
-         
-}
+      )}
+    </>
+  );
+};
 
 export default ContestSpace;
