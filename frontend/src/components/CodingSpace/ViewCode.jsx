b6d67662fdffdf77282ee9f6f5b3733fb892f2ba
srujangit123 <srujantmk@gmail.com>
2021-10-25 23:09:44 +0530

    Added submissions frontend

---
+++ frontend/src/components/CodingSpace/ViewCode.jsx
@@ -0,0 +1,34 @@
+import React from 'react'
+import 'codemirror/lib/codemirror.css'
+import 'codemirror/theme/material.css'
+import 'codemirror/mode/javascript/javascript'
+import 'codemirror/mode/clike/clike'
+import 'codemirror/mode/python/python'
+import { Controlled as ControlledEditor } from 'react-codemirror2'
+import { Button } from 'react-bootstrap';
+
+const ViewCode = (props) => {
+  const {
+    languageMode,
+    code,
+  } = props
+
+  return (
+    <>
+      <ControlledEditor
+        value={code}
+        className="code-mirror-wrapper"
+        options = {{
+          lineWrapping: true,
+          lint: true,
+          mode: languageMode,
+          theme: 'material',
+          lineNumbers: true,
+          readOnly: true
+        }}
+      />
+    </>
+  )
+}
+
+export default ViewCode;
