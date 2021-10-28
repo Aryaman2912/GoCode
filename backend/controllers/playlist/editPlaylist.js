da5d16650c99c215031c9a2259a98f8adaa6142a
Janmansh <janmansh.191cs205@nitk.edu.in>
2021-10-28 16:19:28 +0530

    playlist backend improved

--- backend/controllers/playlist/editPlaylist.js
+++ backend/controllers/playlist/editPlaylist.js
@@ -4,8 +4,7 @@ import auth from '../../middleware/auth.js'
 // display the current playlist "get" request
 export const showPlaylist = function(req, res){
     try{
-        const {playlistId} = req.params;
-        Playlists.findById(playlistId, function(error, playlist){
+        Playlists.findById(req.params.id, function(error, playlist){
             if(error){
                 res.json({status: "failure"});
             }
@@ -24,10 +23,9 @@ export const showPlaylist = function(req, res){
 // add a problem to the current playlist "post" request
 export const addProblem = function(req, res){
     try{
-        const {playlistId} = req.params;
         const {problem} = req.body;
 
-        Playlists.findById(playlistId, function(error, playlist){
+        Playlists.findById(req.params.id, function(error, playlist){
             if(error){
                 res.json({status: "failure"});
             }
@@ -35,7 +33,30 @@ export const addProblem = function(req, res){
             else{
                 playlist.problems.addToSet(problem);
                 playlist.save();
-                res.send("Problem added!");
+                console.log("Problem added!");
+                //res.send("Problem added!");
+            }
+        });
+    }
+
+    catch(error){
+        res.status(500).json({message: error});
+    }
+};
+
+// change description 
+export const changeDesc = function(req, res){
+    try{
+
+        Playlists.findById(req.params.id, function(error, playlist){
+            if(error){
+                res.json({status: "failure"});
+            }
+
+            else{
+                Playlists.updateOne({ _id: req.params }, { description: req.body.description });
+                console.log("Description updated!");
+                //res.send("Problem added!");
             }
         });
     }
--- backend/controllers/playlist/showPlaylist.js
+++ backend/controllers/playlist/showPlaylist.js
@@ -4,13 +4,13 @@ import auth from '../../middleware/auth.js'
 // display the current playlist "get" request
 export const show = function(req, res){
     try{
-        const {playlistId} = req.params;
-        Playlists.findById(playlistId, function(error, playlist){
+        Playlists.findById(req.params.id, function(error, playlist){
             if(error){
                 res.json({status: "failure"});
             }
 
             else{
+                //console.log(playlist);
                 res.send(playlist);
             }
         });
@@ -41,3 +41,23 @@ export const likePlaylist = function(req, res){
         res.status(500).json({message: error});
     }
 }*/
+
+// comment on the playlist, post request
+export const comment = function(req, res){
+    try{
+        Playlists.findById(req.params.id, function(error, playlist){
+            if(error){
+                res.json({status: "failure"});
+            }
+
+            else{
+                playlist.comments.push(req.body.comment);
+                playlist.save();
+            }
+        });
+    }
+
+    catch(error){
+        res.status(500).json({message: error});
+    }
+};
