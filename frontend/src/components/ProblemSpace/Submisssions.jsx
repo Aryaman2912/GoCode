073ea43bc69d99da9f20d11141a6da0fcacc05a4
Rashmith <rashmiths.191cs242@nitk.edu.in>
2021-10-29 12:52:12 +0530

    my submissions page

--- frontend/src/components/ProblemSpace/Submisssions.jsx
+++ frontend/src/components/ProblemSpace/Submisssions.jsx
@@ -1,7 +1,9 @@
+import { Typography } from "@material-ui/core";
 import { useEffect, useState } from "react";
 import ReactLoading from "react-loading";
 import { useHistory } from "react-router";
 import ViewCode from "../CodingSpace/ViewCode";
+import SubmissionCard from "./SubmissionCard";
 
 const RESOLVER = {
   cpp17: "clike",
@@ -27,7 +29,7 @@ const Submissions = (props) => {
       Authorization: `Bearer ${token}`,
     };
     fetch(
-      `http://localhost:5000/api/submissions?problemID=${props.match.params.id}`,
+      `http://localhost:5000/submissions?problemID=${props.match.params.id}`,
       {
         method: "GET",
         headers: headers,
@@ -48,19 +50,48 @@ const Submissions = (props) => {
   };
   return (
     <>
-      {submissions.map((submission) => {
-        return (
-          <div>
-            <h3>{submission.problemID}</h3>
-            <p>{submission.verdict}</p>
-            <p>{submission.timeStamp}</p>
-            <ViewCode
-              code={submission.code}
-              languageMode={RESOLVER[submission.language]}
-            />
-          </div>
-        );
-      })}
+      {loading ? (
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            height: "90vh",
+          }}
+        >
+          <ReactLoading
+            type={loadingOptions.type}
+            color={loadingOptions.color}
+            height={100}
+            width={100}
+          />
+        </div>
+      ) : (
+        <>
+          <Typography
+            variant="h5"
+            style={{
+              color: "white",
+            }}
+          >
+            My Submissions
+          </Typography>
+          {submissions.map((submission) => {
+            return (
+              // <div>
+              //   <h3>{submission.problemID}</h3>
+              //   <p>{submission.verdict}</p>
+              //   <p>{submission.timeStamp}</p>
+              //   <ViewCode
+              //     code={submission.code}
+              //     languageMode={RESOLVER[submission.language]}
+              //   />
+              // </div>
+              <SubmissionCard submission={submission} />
+            );
+          })}
+        </>
+      )}
     </>
   );
 };
