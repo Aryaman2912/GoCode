49080d0e2ba2de1daf550de22e4f7e93278cf64b
Samarth <samarthmayya@gmail.com>
2021-11-07 20:03:59 +0530

    Added participate links

--- frontend/src/components/ContestSpace/ContestDeleteButton.jsx
+++ frontend/src/components/ContestSpace/ContestDeleteButton.jsx
@@ -1,16 +1,19 @@
 import React, { useEffect } from "react";
 import Button from "@material-ui/core/Button";
 import axios from 'axios';
+import { useHistory } from "react-router";
 import { CurrentUser } from "../../api/CurrentUser";
 
 export default function ContestDeleteButton({ contest }) {
-    const deleteContest = (contestId) => {
+  const history = useHistory();  
+  const deleteContest = (contestId) => {
         // console.log(contestId);
         console.log(`http://localhost:5000/api/contests/${contestId}`);
         axios.delete(`http://localhost:5000/api/contests/${contestId}`)
         .then((res) => {
           if(res.status === 200) {
             alert("Contest Deleted Successfully");
+            history.push('/problems');
             console.log(this);
           }
           else console.log(res);
