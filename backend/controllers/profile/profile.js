f3c9e0f8bf29804739aac62f72e2f3ac15673f49
chethan-hebbar <chethan0hebbar@gmail.com>
2021-11-08 16:48:29 +0530

    Profile space backend rectified

--- backend/controllers/profile/profile.js
+++ backend/controllers/profile/profile.js
@@ -5,23 +5,28 @@ import auth from '../../middleware/auth.js'
 // 'display' GET request
 export const display = function(req, res){
     try{
-        Profiles.findOne({userId: req.userId}, function(error, profile){
+        Users.findById(req.userId, function(error, user){
             if(error){
                 res.json({status: "failure"});
             }
 
             else{
-                //console.log(profile);
-                Users.findById(req.userId, function(error, user){
-                    if(error){
-                        res.json({status: "failure"});
-                    }
-
-                    else{
-                        res.status(200).json({user});
-                    }
-                });
+                res.status(200).json({user});
             }
+        });
+    }
+
+    catch(error){
+        res.status(500).json({message: error});
+    }
+};
+
+// "update" POST request
+export const updateProfile = function(req, res){
+    try{
+        Users.updateOne({_id : req.userId }, {name: req.body.name}, function (err, result) {
+               if (err) throw err;
+               else console.log("Updated user info");
         });
     }
 
--- backend/routes/profile.js
+++ backend/routes/profile.js
@@ -2,8 +2,8 @@ import express from 'express';
 
 const router = express.Router();
 
-import {display} from '../controllers/profile/profile.js';
-import {displayFriends, addFriend} from '../controllers/profile/friends.js';
+import {display, updateProfile} from '../controllers/profile/profile.js';
+import {displayFriends, searchUser, addFriend} from '../controllers/profile/friends.js';
 import {displayProblems} from '../controllers/profile/userProblems.js';
 import {displayGivenContests, displayCreatedContests} from '../controllers/profile/userContests.js';
 import { uploadPhoto } from '../controllers/profile/uploadPhoto.js';
@@ -13,10 +13,13 @@ import multer from 'multer';
 const fileUpload = multer()
 
 router.get("/", auth, display);  //localhost:5000/profile
+router.post("/", auth, updateProfile);
 router.get("/friends", auth, displayFriends);  //localhost:5000/profile/friends
 router.get("/problems", auth, displayProblems);  //localhost:5000/profile/problems
 router.get("/contests", auth, displayGivenContests); // and so on
 router.get("/contests", auth, displayCreatedContests);
+//router.post("/", auth, updateProfile);
+router.post("/friends/search", auth, searchUser);
 router.post("/friends", auth, addFriend);
 router.post("/uploadPhoto", auth, fileUpload.single('image'), uploadPhoto);
 
