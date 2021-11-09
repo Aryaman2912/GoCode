73388ef446634afa2787ea1ee2665556db91c5d4
Rashmith <rashmiths.191cs242@nitk.edu.in>
2021-11-09 11:44:56 +0530

    added copy to clipboard

--- frontend/src/constants/config.js
+++ frontend/src/constants/config.js
@@ -1,2 +1,7 @@
-export const domain = "http://localhost:5000"
-//"https://gocode-nitk.herokuapp.com"
+export const domain =
+    //"http://localhost:5000"
+    "https://gocode-nitk.herokuapp.com"
+
+export const frontendDomain =
+    // "http://localhost:3000"
+    "https://upbeat-minsky-c3cd5b.netlify.app/"
